c786beae56837ef003d9bd35ed8749742a519f48
xinwei3 <xinwei3@illinois.edu>
2017-03-30 12:43:54 -0500

    Added DB
    DB integration added

--- api/annotationAPI.py
+++ api/annotationAPI.py
@@ -2,6 +2,9 @@ from flask import make_response, render_template, current_app, jsonify, request
 from flask_restful import Resource, reqparse
 
 from schema.Annotation import Annotation
+from schema.User import User
+from schema.DataSet import DataSet
+from schema.Query import Query
 
 parser = reqparse.RequestParser()
 parser.add_argument('User', type=str)
@@ -16,7 +19,19 @@ class AnnotationAPI(Resource):
         args = parser.parse_args()
         query = args['Query']
         user = args['User']
-        DataSet = args['DataSet']
+        data_id = args['DataSet']
         doc = args['doc']
         headers = {'Content-Type': 'application/json'}
+        annotator = User.objects(name = user)
+        ds = DataSet.objects(ds_name = data_id)
+        q_id = Query.objects(id = query)
+        for pair in doc:
+            annotation = Annotation()
+            annotation.annotator = annotator
+            annotation.query = q_id
+            annotation.data_set = ds
+            annotation.doc = pair['doc_id']
+            annotation.judgement = bool(pair['judge'])
+            annotation.save()
+
         return make_response(jsonify("succeed"), 200, headers)
