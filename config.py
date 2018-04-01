bb482b5872992f9398fe313d3792983e478c5fa6
Peiyuan Zhao <zpy1995@gmail.com>
2018-03-31 21:50:19 -0500

    Update db config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 DEBUG = True
 SECRET_KEY = 'XINGOD'
-MONGODB_DB = 'virtual_ir_lab_testdb'
-MONGODB_HOST = 'ds058579.mlab.com'
-MONGODB_PORT = 58579
-MONGODB_USERNAME = 'TestDBUser'
-MONGODB_PASSWORD = 'uiuc2017'
+MONGODB_DB = 'virlab'
+MONGODB_HOST = 'ds147069.mlab.com'
+MONGODB_PORT = 47069
+MONGODB_USERNAME = 'admin'
+MONGODB_PASSWORD = 'admin'
 REDIS_URL = "redis://pub-redis-19720.us-west-2-1.1.ec2.garantiadata.com:19720"
